237135be6d0365f2a51e6c0c7f01dbc1a642799e
lcarbonn <laurent.carbonnaux@gmail.com>
2021-10-10 12:59:01 +0200

    change (c)

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="AppFooter">
-    <br/>
-    Copyright © 2020 - Made with vapes in Toulouse, France
-  </div>
+    <div class="AppFooter">
+        <br />
+        Copyright © 2021 - Made with vapes in Toulouse, France
+    </div>
 </template>
 
 <style>
 .AppFooter {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 0.8rem;
-  color:white;
-  font-weight: 300;
-  margin: 2px;
-  text-align: center;
+    font-family: "Quicksand", "Source Sans Pro", -apple-system,
+        BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+        sans-serif;
+    font-size: 0.8rem;
+    color: white;
+    font-weight: 300;
+    margin: 2px;
+    text-align: center;
 }
 </style>
